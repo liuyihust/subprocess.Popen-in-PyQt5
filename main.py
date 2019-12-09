374b1ea519808c150fe54558b63cb46858dd60a3
liuyihust <37931180+liuyihust@users.noreply.github.com>
2019-12-09 22:40:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 if __name__ == '__main__':
     import sys
     from PyQt5.QtWidgets import QApplication
+    from processDialog import ProcessDialog
 
     app = QApplication(sys.argv)
-    viewer = processDialog()
+    viewer = ProcessDialog()
     viewer.show()
 
     sys.exit(app.exec_())
